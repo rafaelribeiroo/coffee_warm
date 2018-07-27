586f4222c7f0cd57c46bc3c2f286e037f60e0a07
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-07-26 22:17:08 -0300

    Múltiplas URLs (boas práticas) e alterando o Makefile

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ clean:
 
 # Rodando em todos os pc's conectados na mesma rede
 run:
-	python manage.py runserver 0.0.0.0:8000
+	python manage.py runserver 127.0.0.1:8000
 
 # Migração de banco de dados
 migrate:
--- src/settings.py
+++ src/settings.py
@@ -32,12 +32,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My apps
+    'src.apps.core',
+    'src.apps.blog',
 ]
 
 MIDDLEWARE = [
