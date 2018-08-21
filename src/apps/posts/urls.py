73f69b6e22bf364dfd579d590b351d04c5aedf5a
Rafael Ribeiro <pereiraribeirorafael@gmail.com>
2018-08-20 21:46:04 -0300

    comentarios

--- src/apps/posts/urls.py
+++ src/apps/posts/urls.py
@@ -10,11 +10,16 @@ from .views import (
 
 app_name = 'posts'
 urlpatterns = [
+    # Listagem de resenhas
     path('posts/', post_list, name='homepage'),
+    # Criacao
     path('posts/redigir/', post_create, name='post_create'),
+    # Detalhes
     re_path(r'^post/(?P<slug>[\w-]+)/$', PostDetailView.as_view(), name='detail'),
+    # Edicao
     re_path(r'^post/(?P<slug>[\w-]+)/editar/$', post_update, name='update'),
+    # Criacao de tag
     path('criar/tag/', tag_create, name='tag_create'),
+    # Resenhas por tag
     re_path(r'^post/tag=(?P<tag_slug>[\w-]+)/$', post_by_tag, name='post_by_tag'),
-    # re_path(r'^post/tag=(?P<tag>[\w-]+)/$', post_by_tag, name='post_by_tag'),
 ]
